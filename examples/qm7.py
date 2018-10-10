f3eeed9b3cdd2a81044526438731736f2d87de42
Louis Thiry <biboundai@homail.fr>
2018-10-10 20:21:59 +0200

    MAE 2.9

--- examples/qm7.py
+++ examples/qm7.py
@@ -2,95 +2,86 @@ import numpy as np
 import torch
 from tqdm import tqdm
 from cheml.datasets import load_qm7
-from sklearn import linear_model, model_selection
+from sklearn import linear_model, model_selection, preprocessing, pipeline
 from scipy.spatial.distance import pdist
 from scatharm.scattering import SolidHarmonicScattering
-from scatharm.utils import compute_integrals, generate_weighted_sum_of_gaussians
+from scatharm.utils import generate_weighted_sum_of_gaussians_in_fourier_space
 
 
-def evaluate_bilinear_regression(scat_1, scat_2, target):
-    x = np.concatenate([scat_1, scat_2], axis=1)
-    x = (x - x.mean()) / x.std()
-    y_factor = (target.max() - target.min())
-    y = (target - target.min()) / y_factor
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y)
-
-    x_train = torch.autograd.Variable(torch.from_numpy(x_train), requires_grad=False)
-    x_test = torch.autograd.Variable(torch.from_numpy(x_test), requires_grad=False)
-    y_train = torch.autograd.Variable(torch.from_numpy(y_train), requires_grad=False)
-    y_test = torch.autograd.Variable(torch.from_numpy(y_test), requires_grad=False)
-
-    n_x = x_train.size(1)
-    bilinear = torch.nn.Bilinear(n_x, n_x, 1, bias=False)
-    linear = torch.nn.Linear(n_x, 1)
-    loss_fn = torch.nn.MSELoss(size_average=True)
-    params = list(bilinear.parameters()) + list(linear.parameters())
-
-    learning_rate = 1e-3
-    optimizer = torch.optim.Adam(params, lr=learning_rate)
-
-    for t in range(20000):
-        y_train_pred = bilinear(x_train, x_train) + linear(x_train)
-        loss = loss_fn(y_train_pred, y_train)
-        if (t % 500) == 0:
-            print(t, np.sqrt(loss.data[0])*y_factor)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print('train RMSE : ', np.sqrt(loss.data[0])*y_factor)
-
-    y_test_pred = bilinear(x_test, x_test) + linear(x_test)
-    loss = loss_fn(y_test_pred, y_test)
-    print('test RMSE: ', np.sqrt(loss.data[0])*y_factor)
-
-
-def evaluate_linear_regression(scat, target):
-    alphas = 10.**(-np.arange(5, 15))
-    maes = np.zeros_like(alphas)
-    x_tr, x_te, y_tr, y_te = model_selection.train_test_split()
+def evaluate_linear_regression(scat, target, cross_val_folds, alphas=10.**(-np.arange(0, 10))):
     for i, alpha in enumerate(alphas):
-        ridge = linear_model.Ridge(alpha=alpha)
-        ridge.fit(x_tr, y_tr)
-        maes[i] = np.mean(np.abs(y_te - ridge.predict(x_te)))
-    print('Ridge regression :')
-    for i in range(len(alphas)):
-        print(alphas[i], maes[i])
+        regressor = pipeline.make_pipeline(preprocessing.StandardScaler(), linear_model.Ridge(alpha=alpha))
+        scat_prediction = model_selection.cross_val_predict(regressor, X=scat, y=target, cv=cross_val_folds)
+        scat_MAE = np.mean(np.abs(scat_prediction - target))
+        scat_RMSE = np.sqrt(np.mean((scat_prediction - target)**2))
+        print('Ridge regression, alpha: {}, MAE: {}, RMSE: {}'.format(
+            alpha, scat_MAE, scat_RMSE))
 
 
-def get_valence(charges):
+def get_electron_valences(atomic_numbers):
+    assert np.all(atomic_numbers >= 0), "atomic number must be positive"
+    assert np.all(atomic_numbers < 19), "valence electrons computation not implemented for atomic number gt 18"
     return (
-        charges * (charges <= 2) +
-        (charges - 2) * np.logical_and(charges > 2, charges <= 10) +
-        (charges - 10) * np.logical_and(charges > 8, charges <= 18))
+        atomic_numbers * (atomic_numbers <= 2) +
+        (atomic_numbers - 2) * np.logical_and(atomic_numbers > 2, atomic_numbers <= 10) +
+        (atomic_numbers - 10) * np.logical_and(atomic_numbers > 8, atomic_numbers <= 18))
 
 
-def renormalize(positions, charges, sigma, overlapping_precision=1e-1):
-    min_dist = np.inf
-
-    for i in range(positions.shape[0]):
-        # positions[i,:,0] -= 0.5 * (positions[i,:,0].min() + positions[i,:,0].max())
-        # positions[i,:,1] -= 0.5 * (positions[i,:,1].min() + positions[i,:,1].max())
-        # positions[i,:,2] -= 0.5 * (positions[i,:,2].min() + positions[i,:,2].max())
-        n_atoms = np.sum(charges[i] != 0)
-        pos = positions[i, :n_atoms, :]
-        min_dist = min(min_dist, pdist(pos).min())
-
-    delta = sigma * np.sqrt(-8 * np.log(overlapping_precision))
-    print(delta / min_dist)
-
-    return positions * delta / min_dist
+def get_atom_valences(atomic_numbers):
+    #FIXME: arbitrary choices have been made for N (valence max 5) and Cl (valence max 7)
+    assert np.all(atomic_numbers >= 0), "atomic number must be positive"
+    return (
+        np.isin(atomic_numbers, [1, 3, 9, 17, 11, 19, 37, 55, 87]) * 1 + # H, Li, F, Cl, Na, K, Rb, Cs, Fr
+        np.isin(atomic_numbers, [4, 8, 12, 20, 38]) * 2 + # Be, O, Mg, Ca, Sr
+        np.isin(atomic_numbers, [5, 7, 13, 21, 31, 39]) * 3 + # B, N, Al, Sc, Ga, Y
+        np.isin(atomic_numbers, [6, 14, 22, 32, 40, 50, 72, 82]) * 4 + # C, Si, Ti, Ge, Zr, Sn, Hf, Pb
+        np.isin(atomic_numbers, [15, 23, 33]) * 5 + # P, V, As
+        np.isin(atomic_numbers, [16, 24, 34]) * 6 + # S, Cr, Se
+        np.isin(atomic_numbers, [35, 53]) * 7 # Br, I
+    )
 
 
-def get_qm7_positions_energies_and_charges(M, N, O, J, L, sigma):
+def get_sigma_0_wavelet(sigma, overlapping_precision):
+    return sigma * np.sqrt(-2 * np.log(overlapping_precision) - 1)
+
+
+def get_qm7_energies_and_folds(random_folds=False):
+    qm7 = load_qm7(align=True)
+    energies = qm7.T.transpose()
+    n_folds = qm7.P.shape[0]
+    if random_folds:
+        P = np.random.permutation(energies.shape[0]).reshape((n_folds, -1))
+    else:
+        P = qm7.P
+    cross_val_folds = []
+    for i_fold in range(n_folds):
+        fold = (np.concatenate(P[np.arange(n_folds) != i_fold], axis=0), P[i_fold])
+        cross_val_folds.append(fold)
+    return energies, cross_val_folds
+
+
+def get_qm7_positions_energies_and_charges(M, N, O, J, L, sigma, overlapping_precision):
     qm7 = load_qm7(align=True)
     positions = qm7.R
-    charges = qm7.Z
-    energies = qm7.T.transpose()
-    valence_charges = get_valence(charges)
+    atomic_numbers = qm7.Z
+    atom_valences = get_atom_valences(atomic_numbers)
+    electron_valences = get_electron_valences(atomic_numbers)
 
-    positions = renormalize(positions, charges, sigma)
+    min_dist = np.inf
+    for i in range(positions.shape[0]):
+        n_atoms = np.sum(atomic_numbers[i] != 0)
+        distances = pdist(positions[i, :n_atoms, :])
+        min_dist = min(min_dist, distances.min())
 
-    return torch.from_numpy(positions), torch.from_numpy(energies), torch.from_numpy(charges), torch.from_numpy(valence_charges)
+    delta = sigma * np.sqrt(-8 * np.log(overlapping_precision))
+    print(delta, min_dist)
+
+    positions *= delta / min_dist
+
+    return (
+            torch.from_numpy(positions), torch.from_numpy(atomic_numbers),
+            torch.from_numpy(atom_valences), torch.from_numpy(electron_valences),
+        )
 
 
 # def main():
@@ -100,81 +91,96 @@ def get_qm7_positions_energies_and_charges(M, N, O, J, L, sigma):
 
     # Achieves a MAE of ... kcal.mol-1
     # """
+
 cuda = torch.cuda.is_available()
-batch_size = 32
+batch_size = 16
 M, N, O = 192, 128, 96
-grid = torch.from_numpy(
-        np.fft.ifftshift(np.mgrid[-M//2:-M//2+M, -N//2:-N//2+N, -O//2:-O//2+O].astype('float32'), axes=(1,2,3)))
+
+fourier_grid_np = np.mgrid[-M//2:-M//2+M, -N//2:-N//2+N, -O//2:-O//2+O].astype('float32')
+fourier_grid_np[0] *= 2*np.pi / M
+fourier_grid_np[1] *= 2*np.pi / N
+fourier_grid_np[2] *= 2*np.pi / O
+
+fourier_grid = torch.from_numpy(np.fft.ifftshift(fourier_grid_np, axes=(1,2,3)))
 if cuda:
-    grid = grid.cuda()
+    fourier_grid = fourier_grid.cuda()
+overlapping_precision = 1e-1
 sigma = 1.5
-J, L = 2, 1
-integral_powers = [1.]
+J, L = 2, 3
+integral_powers = [0.5, 1., 2., 3.]
 args = {'integral_powers': integral_powers}
-pos, energies, full_charges, valence_charges = get_qm7_positions_energies_and_charges(
-        M, N, O, J, L, sigma)
+sigma_0 = get_sigma_0_wavelet(sigma, overlapping_precision)
+pos, atomic_numbers, atom_valences, electron_valences = get_qm7_positions_energies_and_charges(
+        M, N, O, J, L, sigma, overlapping_precision)
+
 n_molecules = pos.size(0)
 n_batches = np.ceil(n_molecules / batch_size).astype(int)
 
-scat = SolidHarmonicScattering(M=M, N=N, O=O, J=J, L=L, sigma_0=sigma)
+scat = SolidHarmonicScattering(M=M, N=N, O=O, J=J, L=L, sigma_0=sigma_0)
 
 scat_0, scat_1, scat_2 = [], [], []
-print('Computing solid harmonic scattering coefficients of molecules \
-        of QM7 database on {}'.format('GPU' if cuda else 'CPU'))
+print('Computing solid harmonic scattering coefficients of molecules '
+      'of QM7 database on {}'.format('GPU' if cuda else 'CPU'))
+print('batch_size: {}, n_batches: {}'.format(batch_size, n_batches))
 print('L: {}, J: {}, integral powers: {}'.format(L, J, integral_powers))
+
 for i in tqdm(range(n_batches)):
     start, end = i*batch_size, min((i+1)*batch_size, n_molecules)
 
     pos_batch = pos[start:end]
-    full_batch = full_charges[start:end]
 
-    full_density_batch = generate_weighted_sum_of_gaussians(
-            grid, pos_batch, full_batch, sigma, cuda=cuda)
+    # atomic number channel
+    atomic_number_batch = atomic_numbers[start:end]
+    atomic_number_fourier_density_batch = generate_weighted_sum_of_gaussians_in_fourier_space(
+            fourier_grid, pos_batch, atomic_number_batch, sigma, cuda=cuda)
+    atomic_number_scat_0, atomic_number_scat_1, atomic_number_scat_2 = scat(
+            atomic_number_fourier_density_batch, fourier_input=True,
+            order_2=True, method='integral', method_args=args)
 
-    full_scat_0 = compute_integrals(full_density_batch, integral_powers)
-    full_scat_1, full_scat_2 = scat(full_density_batch, order_2=True,
-                                    method='integral', method_args=args)
+    # # atom valence batch
+    # atom_valence_batch = atom_valences[start:end]
+    # atom_valence_density_batch = generate_weighted_sum_of_gaussians(
+            # grid, pos_batch, atom_valence_batch, sigma, cuda=cuda)
+    # atom_valence_scat_0 = compute_integrals(atom_valence_density_batch, integral_powers)
+    # atom_valence_scat_1, atom_valence_scat_2 = scat(atom_valence_density_batch, order_2=True,
+                                  # method='integral', method_args=args)
 
-    full_scat_0 = compute_integrals(full_density_batch, integral_powers)
-    val_batch = valence_charges[start:end]
-    val_density_batch = generate_weighted_sum_of_gaussians(
-            grid, pos_batch, val_batch, sigma, cuda=cuda)
-    val_scat_0 = compute_integrals(val_density_batch, integral_powers)
-    val_scat_1, val_scat_2 = scat(val_density_batch, order_2=True,
-                                  method='integral', method_args=args)
+    # # electron valence batch
+    electron_valence_batch = electron_valences[start:end]
+    electron_valence_fourier_density_batch = generate_weighted_sum_of_gaussians_in_fourier_space(
+            fourier_grid, pos_batch, electron_valence_batch, sigma, cuda=cuda)
+    electron_valence_scat_0, electron_valence_scat_1, electron_valence_scat_2 = scat(
+            electron_valence_fourier_density_batch, fourier_input=True,
+            order_2=True, method='integral', method_args=args)
 
-    core_density_batch = full_density_batch - val_density_batch
-    core_scat_0 = compute_integrals(core_density_batch, integral_powers)
-    core_scat_1, core_scat_2 = scat(core_density_batch, order_2=True,
-                                    method='integral', method_args=args)
+    # # old core channel
+    core_fourier_density_batch = atomic_number_fourier_density_batch - electron_valence_fourier_density_batch
+    core_scat_0, core_scat_1, core_scat_2 = scat(
+            core_fourier_density_batch, fourier_input=True, order_2=True,
+            method='integral', method_args=args)
 
-    scat_0.append(torch.stack([full_scat_0, val_scat_0, core_scat_0], dim=-1))
-    scat_1.append(torch.stack([full_scat_1, val_scat_1, core_scat_1], dim=-1))
-    scat_2.append(torch.stack([full_scat_2, val_scat_2, core_scat_2], dim=-1))
+    scat_0.append(torch.cat([atomic_number_scat_0, electron_valence_scat_0, core_scat_0], dim=-1))
+
+    scat_1.append(torch.stack([atomic_number_scat_1, electron_valence_scat_1, core_scat_1], dim=-1))
+    scat_2.append(torch.stack([atomic_number_scat_2, electron_valence_scat_2, core_scat_2], dim=-1))
 
 scat_0 = torch.cat(scat_0, dim=0)
 scat_1 = torch.cat(scat_1, dim=0)
 scat_2 = torch.cat(scat_2, dim=0)
 
-np_scat_0 = scat_0.numpy().reshape((n_molecules, -1))
-np_scat_1 = scat_1.numpy().reshape((n_molecules, -1))
-np_scat_2 = scat_2.numpy().reshape((n_molecules, -1))
+print('scattering coefficients :')
+print('order 0, shape {} = (n_molecules, n_powers, n_channels)'.format(scat_0.size()))
+print('order 1, shape {} = (n_molecules, n_powers, J+1, L+1, n_channels)'.format(scat_1.size()))
+print('order 2, shape {} = (n_molecules, n_powers, J(J+1)/2, L+1, n_channels)'.format(scat_1.size()))
 
-basename = 'qm7_L_{}_J_{}_sigma_{}_MNO_{}_powers_{}.npy'.format(
-        L, J, sigma, (M, N, O), integral_powers)
-np.save('scat_0_' + basename, np_scat_0)
-np.save('scat_1_' + basename, np_scat_1)
-np.save('scat_2_' + basename, np_scat_2)
+np_scat_0 = scat_0.numpy().reshape((end, -1))
+np_scat_1 = scat_1.numpy().reshape((end, -1))
+np_scat_2 = scat_2.numpy().reshape((end, -1))
 
-scat = np.concatenate([np_scat_0, np_scat_1, np_scat_2], axis=1)
-target = energies.numpy()
+scat_0_1_2 = np.concatenate([np_scat_0, np_scat_1, np_scat_2], axis=1)
 
-print('order 1 : {} coef, order 2 : {} coefs'.format(np_scat_1.shape[1], np_scat_2.shape[1]))
-
-evaluate_linear_regression(scat, target)
-
-evaluate_bilinear_regression(np_scat_1, np_scat_2, target)
-
+energies, cross_val_folds = get_qm7_energies_and_folds()
+evaluate_linear_regression(scat_0_1_2, energies, cross_val_folds)
 
 # if __name__ == '__main__':
     # main()
