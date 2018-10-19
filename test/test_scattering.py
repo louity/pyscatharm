55c87cd84cb2a6feb6bc7dd28bce4e7f9b137869
louity <louis.thiry@outlook.fr>
2018-10-19 10:51:54 +0200

    fix test for non CUDA computers and made them compatible with pytorch 0.4

--- test/test_scattering.py
+++ test/test_scattering.py
@@ -7,13 +7,17 @@ from scatharm.filters_bank import gaussian_3d, solid_harmonic_filters_bank
 from scatharm.scattering import SolidHarmonicScattering
 from scatharm import utils as sl
 
+gpu_flags = [False]
+if torch.cuda.is_available():
+    gpu_flags.append(True)
+
 def linfnorm(x,y):
     return torch.max(torch.abs(x-y))
 
 class TestScattering(unittest.TestCase):
     def testFFT3dCentralFreqBatch(self):
         # Checked the 0 frequency for the 3D FFT
-        for gpu in [False, True]:
+        for gpu in gpu_flags:
             x = torch.FloatTensor(1, 32, 32, 32, 2).fill_(0)
             if gpu:
                 x = x.cuda()
@@ -22,7 +26,7 @@ class TestScattering(unittest.TestCase):
             fft3d = sl.Fft3d()
             y = fft3d(x)
             c = y[:,0,0,0].sum()
-            self.assertAlmostEqual(a, c, places=6)
+            self.assertAlmostEqual(float(a), float(c), places=6)
 
     def testSumOfGaussianFFT3d(self):
         # Check the validity of Fourier transform of sum of gaussians
@@ -37,9 +41,7 @@ class TestScattering(unittest.TestCase):
         np_fourier_grid[1] *= 2*np.pi / N
         np_fourier_grid[2] *= 2*np.pi / O
         grid = torch.from_numpy(np_grid)
-        grid_gpu = grid.cuda()
         fourier_grid = torch.from_numpy(np_fourier_grid)
-        fourier_grid_gpu = fourier_grid.cuda()
 
         positions = torch.FloatTensor(1, n_gaussians, 3).uniform_(-0.5 * _N + 5*sigma, 0.5 * _N - 5*sigma)
         positions[...,2].fill_(0)
@@ -47,10 +49,10 @@ class TestScattering(unittest.TestCase):
 
         fft3d = sl.Fft3d()
 
-        for gpu in [False, True]:
+        for gpu in gpu_flags:
             if gpu:
-                _grid = grid_gpu
-                _fourier_grid = fourier_grid_gpu
+                _grid = grid.cuda()
+                _fourier_grid = fourier_grid.cuda()
             else:
                 _grid = grid
                 _fourier_grid = fourier_grid
@@ -59,7 +61,8 @@ class TestScattering(unittest.TestCase):
             sum_of_gauss_fourier = sl.generate_weighted_sum_of_gaussians_in_fourier_space(
                 _fourier_grid, positions, weights, sigma, cuda=gpu)
             sum_of_gauss_ = fft3d(sum_of_gauss_fourier, inverse=True, normalized=True)[..., 0]
-            self.assertAlmostEqual(torch.norm(sum_of_gauss - sum_of_gauss_), 0, places=5)
+            difference = float(torch.norm(sum_of_gauss - sum_of_gauss_))
+            self.assertAlmostEqual(difference, 1, places=5)
 
 
     def testSolidHarmonicFFT3d(self):
@@ -70,7 +73,7 @@ class TestScattering(unittest.TestCase):
         solid_harmonics = solid_harmonic_filters_bank(M, N, O, j_values, L, sigma, fourier=False)
         solid_harmonics_fourier = solid_harmonic_filters_bank(M, N, O, j_values, L, sigma, fourier=True)
         fft3d = sl.Fft3d()
-        for gpu in [False, True]:
+        for gpu in gpu_flags:
             for l in range(L+1):
                 for m in range(2*l+1):
                     solid_harm = solid_harmonics[l][0:1,m]
@@ -79,7 +82,8 @@ class TestScattering(unittest.TestCase):
                         solid_harm = solid_harm.cuda()
                         solid_harm_fourier = solid_harm_fourier.cuda()
                     solid_harm_ = fft3d(solid_harm_fourier, inverse=True, normalized=True)
-                    self.assertAlmostEqual(torch.norm(solid_harm - solid_harm_), 0, places=7)
+                    difference = float(torch.norm(solid_harm - solid_harm_))
+                    self.assertAlmostEqual(difference, 0, places=7)
 
 
     def testSolidHarmonicScattering(self):
@@ -100,7 +104,7 @@ class TestScattering(unittest.TestCase):
             sigma_wavelet = sigma_0_wavelet*2**j
             k = sigma_wavelet / np.sqrt(sigma_wavelet**2 + sigma_gaussian**2)
             for l in range(1, L+1):
-                self.assertAlmostEqual(s_order_1[0, 0, i_j, l], k**l, places=4)
+                self.assertAlmostEqual(float(s_order_1[0, 0, i_j, l]), k**l, places=4)
 
 
     def testLowPassFilter(self):
@@ -123,7 +127,7 @@ class TestScattering(unittest.TestCase):
             true_convolved_gaussian = torch.FloatTensor(1, M, N, O, 2).fill_(0)
             true_convolved_gaussian[0, ..., 0] = torch.from_numpy(gaussian_3d(M, N, O, sigma_convolved_gaussian, fourier=False))
 
-            diff = torch.norm(convolved_gaussian - true_convolved_gaussian)
+            diff = float(torch.norm(convolved_gaussian - true_convolved_gaussian))
             self.assertAlmostEqual(diff, 0, places=5)
 
 
