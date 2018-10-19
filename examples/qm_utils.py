683739c2584a8aa99c9f4bd60fee66ba52db6c9f
louity <louis.thiry@outlook.fr>
2018-10-19 10:54:53 +0200

    handle cheml ImportError and show message to the user

--- examples/qm_utils.py
+++ examples/qm_utils.py
@@ -1,9 +1,15 @@
 import numpy as np
 import torch
-from cheml.datasets import load_qm7, load_qm9
 from sklearn import linear_model, model_selection, preprocessing, pipeline, kernel_ridge
 from scipy.spatial.distance import pdist
 
+try:
+    from cheml.datasets import load_qm7, load_qm9
+except ImportError as err:
+    print('Error importing cheml {}. '
+        'You need to have cheml installed to run qm7/9 example : '
+        'https://www.github.com/CheML/CheML'.format(err)) 
+    exit()
 
 def evaluate_regression(scat, target, cross_val_folds, kind='linear', alphas=10.**(-np.arange(0, 10))):
     for i, alpha in enumerate(alphas):
